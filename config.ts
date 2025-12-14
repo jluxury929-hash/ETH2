0ff563325d3dc666cb3485bb0262b01fac5e4644
Josh Platt <jluxury929@gmail.com>
2025-12-13 23:14:55 -0500

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,8 @@
+// config.ts
+// Configuration loading utility. Most simple projects just use dotenv directly.
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// The main bot config object definition (for export to other files)
+// For simplicity, its definition is included in types.ts now.
